8000b91c73e451e01bb1873caea220d0eab5a1e9
Suzana Tesovic <suzanatesovic03@gmail.com>
2025-10-18 12:03:49 +0200

    implementirana je logika za register u AuthControlleru

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,36 +44,5 @@ class AuthController extends Controller
         ], 201);
     }
 
-
-
-    public function login(Request $request)
-    {
-        $request->validate(['email' => 'required|email', 'password' => 'required']);
-
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['Invalid credentials.'],
-            ]);
-        }
-
-        $user->tokens()->delete();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Login successful.',
-            'access_token' => $token,
-            'user' => $user->load('team'),
-        ]);
-    }
-
-    public function logout(Request $request)
-    {
-        $request->user()->currentAccessToken()->delete();
-
-        return response()->json([
-            'message' => 'Logged out successfully. Token revoked.'
-        ]);
-    }
+    
 }
